c620cf961e69cac684c3bb1ad26e5e4439f7e012
Alimjoo <alimjoo00@163.com>
2023-08-21 12:07:38 +0800

    fix export

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -476,9 +476,7 @@ export function ImagePreviewer(props: {
 
           <div>
             <div className={styles["main-title"]}>Piyazon Chat</div>
-            <div className={styles["sub-title"]}>
-              github.com/Yidadaa/ChatGPT-Next-Web
-            </div>
+            <div className={styles["sub-title"]}>https://chat.piyazon.top/</div>
             <div className={styles["icons"]}>
               <ExportAvatar avatar={config.avatar} />
               <span className={styles["icon-space"]}>&</span>
